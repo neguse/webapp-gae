7cba82ab0c1aaf18624b947651f6c4cf1918d32d
neguse <negcee@gmail.com>
2016-01-10 18:43:02 +0900

    add static omikuji

--- hello.go
+++ hello.go
@@ -1,14 +1,46 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+	"net/http"
+
+	"html/template"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/omikuji", omikujiHandler)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+var rootTemplate = template.Must(template.New("root").Parse(`
+<!Doctype HTML>
+<html>
+	<head>
+		<meta charset=utf-8>
+		<title></title>
+	</head>
+	<body>
+		<a href="/omikuji">おみくじ</a>
+	</body>
+</html>
+`))
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	rootTemplate.Execute(w, nil)
+}
+
+var omikujiTemplate = template.Must(template.New("omikuji").Parse(`
+<!Doctype HTML>
+<html>
+	<head>
+		<meta charset=utf-8>
+		<title></title>
+	</head>
+	<body>
+		 大吉です!
+	</body>
+</html>
+`))
+
+func omikujiHandler(w http.ResponseWriter, r *http.Request) {
+	omikujiTemplate.Execute(w, nil)
 }
